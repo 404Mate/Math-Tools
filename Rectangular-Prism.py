9ac0edfeedee9d4a470d6c9af03d5779d1f98c3f
404Mate <cadenstolz05@Gmail.com>
2022-09-23 10:28:24 -0500

    Make newlines \n, add cone volume

--- Rectangular-Prism.py
+++ Rectangular-Prism.py
@@ -1,27 +1,22 @@
-length = input("""what is your length?
-""")
-width = input("""
-what is your width?
-""")
-hight = input("""
-what is your hight?
-""")
+# get dimensions
+length = input("what is your length?\n")
+width = input("\nwhat is your width?\n")
+height = input("\ninputwhat is your height?\n")
 
-hight = float(hight)
+# convert it to floats incase there are decimal points in the input
+height = float(height)
 width = float(width)
 length = float(length)
 
-part1 = width * hight * 2
+# math time
+part1 = width * height * 2
 part2 = width * length * 2
-part3 = length * hight * 2
+part3 = length * height * 2
 
 surfacearea = part1 + part2 + part3
-print("""
-
-
-""")
-print("your volume is: ", length * width * hight)
-print("your surface area is: ", surfacearea)
+print("\n")
+print("your volume is:", length * width * height)
+print("your surface area is:", surfacearea)
 
 #---------------------------
 #  Made by 404Mate with <3  
---
+++ cone-volume.py
@@ -0,0 +1,16 @@
+radius = input("what is your radius?\n")
+radius = int(radius)
+height = input("\nwhat is your height?\n")
+height = float(height)
+
+part1 = radius * radius
+
+base = part1 * 3.14
+
+print("\n")
+print("Your Volume is: ", base * height / 3)
+
+
+#---------------------------
+#  Made by 404Mate with <3  
+#--------------------------- 
