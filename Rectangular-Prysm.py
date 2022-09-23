7334f22c4ce247ca2f764cec195c8b941e8022ba
404Mate <cadenstolz05@Gmail.com>
2022-09-22 19:51:50 -0500

    Initial commit

---
+++ Rectangular-Prysm.py
@@ -0,0 +1,28 @@
+length = input("""what is your length?
+""")
+width = input("""
+what is your width?
+""")
+hight = input("""
+what is your hight?
+""")
+
+hight = float(hight)
+width = float(width)
+length = float(length)
+
+part1 = width * hight * 2
+part2 = width * length * 2
+part3 = length * hight * 2
+
+surfacearea = part1 + part2 + part3
+print("""
+
+
+""")
+print("your volume is: ", length * width * hight)
+print("your surface area is: ", surfacearea)
+
+#---------------------------
+#  Made by 404Mate with <3  
+#--------------------------- 
