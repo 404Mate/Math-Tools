fadb43baaf73f1a4b69b3df56a3038753fe72cf6
404Mate <cadenstolz05@Gmail.com>
2022-09-29 09:04:54 -0500

    Make prints more consistant with case and newlines

--- math-tool.py
+++ math-tool.py
@@ -10,32 +10,31 @@ toolselect = input("""What tool do you need?
 
 if toolselect == "1":
     radius = float(input("What is your radius?\n"))
-    print("your circles area is", radius ** 2 * 3.14, "\n")
-    print("your circles circumfrence is", 2 * radius * 3.14, "\n")
+    print("Your circle's area is", radius ** 2 * 3.14, "\n")
+    print("Your circle's circumfrence is", 2 * radius * 3.14, "\n")
 elif toolselect == "2":
-    radius = float(input("what is your radius?\n"))
-    height = float(input("what is your height?\n"))
-    print("Your Volume is: ", radius ** 2 * 3.14 * height / 3)
+    radius = float(input("What is your radius?\n"))
+    height = float(input("What is your height?\n"))
+    print("Your volume is: ", radius ** 2 * 3.14 * height / 3, "\n")
 elif toolselect == "3":
     radius = float(input("What is your radius?\n"))
     height  = float(input("What is your height?\n"))
-    print("Your surface area is:", height * 2 * radius * 3.14 + (2 * 3.14 * radius ** 2))
+    print("Your surface area is:", height * 2 * radius * 3.14 + (2 * 3.14 * radius ** 2), "\n")
 elif toolselect == "4":
     x1 = float(input("X1\n"))
     y1 = float(input("Y1\n"))
     x2 = float(input("X2\n"))
     y2 = float(input("Y2\n"))
-    print("The distance is the square root of", (x2 - x1) ** 2 + (y2 - y1) ** 2, "or", sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+    print("The distance is the square root of", (x2 - x1) ** 2 + (y2 - y1) ** 2, "or", sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), "\n")
 elif toolselect == "5":
-    length = float(input("what is your length?\n"))
-    width = float(input("\nwhat is your width?\n"))
-    height = float(input("\ninputwhat is your height?\n"))
-    print("your volume is:", length * width * height)
-    print("your surface area is:", (width * height * 2) + (width * length * 2) + (length * height * 2))
+    length = float(input("What is your length?\n"))
+    width = float(input("What is your width?\n"))
+    height = float(input("What is your height?\n"))
+    print("Your volume is:", length * width * height, "\n")
+    print("Your surface area is:", (width * height * 2) + (width * length * 2) + (length * height * 2), "\n")
 else:
     print("Sorry that was not recognized\n")
     exit()
-
 
 
 #---------------------------
